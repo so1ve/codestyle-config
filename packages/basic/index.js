a4fe2b1286383b87ebfb105e6dd3adc3b9fc9244
so1ve <nn_201312@163.com>
2022-04-09 17:35:18 +0800

    feat: update brace-style

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -160,7 +160,7 @@ module.exports = {
     "no-unused-vars": "warn",
     "no-param-reassign": "off",
     "array-bracket-spacing": ["error", "never"],
-    "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+    "brace-style": ["error", "1tbs", { allowSingleLine: true }],
     "block-spacing": ["error", "always"],
     "camelcase": "off",
     "comma-spacing": ["error", { before: false, after: true }],
