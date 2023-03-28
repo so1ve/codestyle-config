31990b17122211701ae43124325ce7b26af1bbab
ʀᴀʏ <i@mk1.io>
2023-03-28 15:31:57 +0800

    fix: fix #3

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -89,7 +89,7 @@ module.exports = defineConfig({
     "@typescript-eslint/member-delimiter-style": "error",
     "@typescript-eslint/type-annotation-spacing": ["error", {}],
     "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports", disallowTypeAnnotations: false }],
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
     "@typescript-eslint/consistent-indexed-object-style": ["error", "record"],
     "@typescript-eslint/prefer-ts-expect-error": "error",
 
