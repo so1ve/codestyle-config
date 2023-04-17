76ae91cee46382e7a0c2da38359dc606bd154e9e
so1ve <i@mk1.io>
2023-04-17 17:07:09 +0800

    feat: more unicorns!

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -406,30 +406,42 @@ module.exports = defineConfig({
     "no-return-assign": "off",
 
     // unicorns
-    // Pass error message when throwing errors
     "unicorn/error-message": "error",
-    // Uppercase regex escapes
     "unicorn/escape-case": "error",
-    // Array.isArray instead of instanceof
     "unicorn/no-instanceof-array": "error",
-    // Prevent deprecated `new Buffer()`
     "unicorn/no-new-buffer": "error",
-    // Keep regex literals safe!
     "unicorn/no-unsafe-regex": "off",
-    // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
     "unicorn/number-literal-case": "error",
-    // includes over indexOf when checking for existence
     "unicorn/prefer-includes": "error",
-    // String methods startsWith/endsWith instead of more complicated stuff
     "unicorn/prefer-string-starts-ends-with": "error",
-    // textContent instead of innerText
     "unicorn/prefer-text-content": "error",
-    // Enforce throwing type error when throwing error while checking typeof
     "unicorn/prefer-type-error": "error",
-    // Enforce adding `node:` protocol
     "unicorn/prefer-node-protocol": "error",
-    // Use new when throwing error
     "unicorn/throw-new-error": "error",
+    "unicorn/no-useless-spread": "error",
+    "unicorn/prefer-spread": "error",
+    "unicorn/prefer-set-size": "error",
+    "unicorn/prefer-string-slice": "error",
+    "unicorn/relative-url-style": ["error", "always"],
+    "unicorn/empty-brace-spaces": "error",
+    "unicorn/explicit-length-check": "error",
+    "unicorn/new-for-builtins": "error",
+    "unicorn/no-array-for-each": "error",
+    "unicorn/no-array-method-this-argument": "error",
+    "unicorn/no-for-loop": "error",
+    "unicorn/no-lonely-if": "error",
+    "unicorn/no-negated-condition": "error",
+    "unicorn/switch-case-braces": "error",
+    "unicorn/prefer-ternary": "error",
+    "unicorn/prefer-query-selector": "error",
+    "unicorn/prefer-modern-dom-apis": "error",
+    "unicorn/prefer-modern-math-apis": "error",
+    "unicorn/prefer-json-parse-buffer": "error",
+    "unicorn/prefer-date-now": "error",
+    "unicorn/prefer-array-some": "error",
+    "unicorn/prefer-array-index-of": "error",
+    "unicorn/prefer-array-flat": "error",
+    "unicorn/prefer-array-find": "error",
 
     "no-use-before-define": ["error", { functions: false, classes: false, variables: true }],
     "eslint-comments/disable-enable-pair": "off",
