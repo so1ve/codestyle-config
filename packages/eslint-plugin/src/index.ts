ef43dcbce4a5299c248dc3a6bf3e02396134f056
so1ve <i@mk1.io>
2023-05-07 18:16:20 +0800

    feat: remove no-beginning-newline rule
    prettier handles that

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,7 +1,6 @@
 import importDedupe from "./rules/import-dedupe";
 import noInlineTypeImport from "./rules/no-inline-type-import";
 import noUselessTemplateString from "./rules/no-useless-template-string";
-import noBeginningNewline from "./rules/no-beginning-newline";
 import functionStyle from "./rules/function-style";
 
 export default {
@@ -9,7 +8,6 @@ export default {
     "import-dedupe": importDedupe,
     "no-inline-type-import": noInlineTypeImport,
     "no-useless-template-string": noUselessTemplateString,
-    "no-beginning-newline": noBeginningNewline,
     "function-style": functionStyle,
   },
 };
