eabd181e00487be20a43f9611e4261ed3ecee1bb
so1ve <i@mk1.io>
2023-05-09 18:08:31 +0800

    Revert "Revert "feat: remove no-beginning-newline rule""
    This reverts commit 5ca71a97417db30088010d4e6d2b8d89638eee88.

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -3,15 +3,13 @@ import importDedupe from "./rules/import-dedupe";
 import noInlineTypeImport from "./rules/no-inline-type-import";
 import noNegatedComparison from "./rules/no-negated-comparison";
 import noUselessTemplateString from "./rules/no-useless-template-string";
-import noBeginningNewline from "./rules/no-beginning-newline";
 
 export default {
   rules: {
     "function-style": functionStyle,
     "import-dedupe": importDedupe,
     "no-inline-type-import": noInlineTypeImport,
-    "no-negated-comparison": noNegatedComparison,
     "no-useless-template-string": noUselessTemplateString,
-    "no-beginning-newline": noBeginningNewline,
+    "no-negated-comparison": noNegatedComparison,
   },
 };
