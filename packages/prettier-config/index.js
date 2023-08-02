39449e4dfa9fcfe8f55ae241d4a96a7223e20a4f
so1ve <nn_201312@163.com>
2023-08-02 19:35:25 +0800

    revert: revert "fix!: damn it! I hate tabs!"
    This reverts commit 64c20bb396161b7ca92a5fa18f0fe6ad9ff7cc35.

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,32 +1,32 @@
 // @ts-check
 
 const plugins = ["astro", "toml", "curly-and-jsdoc", "pkgsort"].map((p) =>
-  require.resolve(`prettier-plugin-${p}`),
+	require.resolve(`prettier-plugin-${p}`),
 );
 
 module.exports = {
-  useTabs: false,
-  quoteProps: "preserve",
-  trailingComma: "all",
-  htmlWhitespaceSensitivity: "ignore",
-  overrides: [
-    {
-      files: "*.html",
-      options: {
-        parser: "angular",
-      },
-    },
-    {
-      files: ["*.json", "*.json5", "*.jsonc", ".eslintrc"],
-      options: {
-        trailingComma: "none",
-      },
-    },
-  ],
-  plugins,
+	useTabs: true,
+	quoteProps: "preserve",
+	trailingComma: "all",
+	htmlWhitespaceSensitivity: "ignore",
+	overrides: [
+		{
+			files: "*.html",
+			options: {
+				parser: "angular",
+			},
+		},
+		{
+			files: ["*.json", "*.json5", "*.jsonc", ".eslintrc"],
+			options: {
+				trailingComma: "none",
+			},
+		},
+	],
+	plugins,
 
-  // Plugin Options
-  // JSDoc
-  jsdocPreferCodeFences: true,
-  tsdoc: true,
+	// Plugin Options
+	// JSDoc
+	jsdocPreferCodeFences: true,
+	tsdoc: true,
 };
