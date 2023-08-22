27c46b3f29112adef2f3743141d7a83cf624c042
Ray <i@mk1.io>
2023-08-22 20:58:13 +0800

    feat: auto JS fallback (#89)

--- packages/eslint-config-solid/index.js
+++ packages/eslint-config-solid/index.js
@@ -1,10 +1,20 @@
 // @ts-check
 const { defineConfig } = require("eslint-define-config");
+const { isPackageExists } = require("local-pkg");
+
+const TS = isPackageExists("typescript");
+
+if (!TS) {
+	console.warn(
+		"[@so1ve/eslint-config-solid] TypeScript is not installed, fallback to JS only.",
+	);
+}
 
 module.exports = defineConfig({
 	extends: [
-		"@so1ve/eslint-config-ts",
-		"plugin:solid/typescript",
+		...(TS
+			? ["@so1ve/eslint-config-ts", "plugin:solid/typescript"]
+			: ["@so1ve/eslint-config-basic"]),
 		"plugin:solid/recommended",
 	],
 });
