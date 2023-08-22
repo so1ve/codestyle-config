af897b9f8a1da8a65c28a8da35de85458375fa85
Ray <i@mk1.io>
2023-08-22 12:04:47 +0800

    perf: enable vitest rules on test specs only

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -169,7 +169,22 @@ module.exports = defineConfig({
 			files: ["*.test.ts", "*.test.js", "*.spec.ts", "*.spec.js"],
 			rules: {
 				"no-unused-expressions": "off",
-				"no-only-tests/no-only-tests": "error",
+				"no-only-tests/no-only-tests": "error",		"vitest/expect-expect": "off",
+		"vitest/valid-describe-callback": "off",
+		"vitest/no-alias-methods": "error",
+		"vitest/no-interpolation-in-snapshots": "error",
+		"vitest/no-test-prefixes": "error",
+		"vitest/prefer-expect-resolves": "error",
+		"vitest/prefer-comparison-matcher": "error",
+		"vitest/prefer-mock-promise-shorthand": "error",
+		"vitest/prefer-spy-on": "error",
+		"vitest/prefer-to-be-falsy": "error",
+		"vitest/prefer-to-be-object": "error",
+		"vitest/prefer-to-be-truthy": "error",
+		"vitest/prefer-to-contain": "error",
+		"vitest/prefer-to-have-length": "error",
+		"vitest/prefer-todo": "error",
+		"vitest/valid-title": ["error", { allowArguments: true }],
 			},
 		},
 		{
@@ -209,24 +224,7 @@ module.exports = defineConfig({
 		// array-func
 		"array-func/prefer-array-from": "off",
 
-		// vitest
-		"vitest/expect-expect": "off",
-		"vitest/valid-describe-callback": "off",
-		"vitest/no-alias-methods": "error",
-		"vitest/no-interpolation-in-snapshots": "error",
-		"vitest/no-test-prefixes": "error",
-		"vitest/prefer-expect-resolves": "error",
-		"vitest/prefer-comparison-matcher": "error",
-		"vitest/prefer-mock-promise-shorthand": "error",
-		"vitest/prefer-spy-on": "error",
-		"vitest/prefer-to-be-falsy": "error",
-		"vitest/prefer-to-be-object": "error",
-		"vitest/prefer-to-be-truthy": "error",
-		"vitest/prefer-to-contain": "error",
-		"vitest/prefer-to-have-length": "error",
-		"vitest/prefer-todo": "error",
-
-		"vitest/valid-title": ["error", { allowArguments: true }],
+		
 
 		// json-schema-validator
 		"json-schema-validator/no-invalid": "warn",
